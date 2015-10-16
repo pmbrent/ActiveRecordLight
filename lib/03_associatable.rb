34d529ba18bd81aa622dccd3cf1cb08d2e7fefaa
pmbrent <patriciambrent@gmail.com>
2015-10-16 14:33:41 -0400

    Implement default keys for belongs_to/has_many

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,23 +10,27 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @class_name = options[:class_name] || name.to_s.camelcase
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
+    @foreign_key = options[:foreign_key] || "#{self_class_name.underscore}_id".to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
