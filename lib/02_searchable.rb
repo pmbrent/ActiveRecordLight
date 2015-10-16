f98c48cd26a5f01102b1a9a9c0dec567c0178668
pmbrent <patriciambrent@gmail.com>
2015-10-16 14:16:11 -0400

    Create module Searchable to mixin where clause searching

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,27 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line(params)}
+    SQL
+
+    results.map { |row| self.new(row) }
+  end
+
+  def where_line(params)
+    conditions = params.keys.map do |key|
+      "#{key} = ?"
+    end
+
+    conditions.join(" AND ")
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
